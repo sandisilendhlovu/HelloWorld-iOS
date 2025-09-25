d88335229a03886594d887bd92be0d3e67d27dbd
Sandy <74529700+sandisilendhlovu@users.noreply.github.com>
2025-09-25 16:48:15 +0200

    Added looping fade-in/out animation to Hello World text to improve app interactivity

--- HelloWorld/ContentView.swift
+++ HelloWorld/ContentView.swift
@@ -8,10 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isVisible = false
+    
     var body: some View {
-        Text("Hello World! 🚀")
-            .font(.largeTitle)
-            .padding()
+        ZStack {
+            Color.black
+                .ignoresSafeArea()
+            
+            Text("Hello World! 🚀")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .foregroundColor(.white)
+                .opacity(isVisible ? 1 : 0)
+                .animation(
+                .easeInOut(duration: 2.0)
+                .repeatForever(autoreverses: true),
+                value: isVisible)
+        }
+        
+        .onAppear {
+            isVisible = true
+        }
     }
 }
 
